717cd1ae38e017d1c5e0f8763d1f3a6404f275a4
Syed <72160793+ssa17@users.noreply.github.com>
2024-12-06 14:46:09 +0000

    Start day 1

---
+++ src/main/java/aoc/Day01.java
@@ -0,0 +1,29 @@
+package aoc;
+
+import java.util.List;
+
+public class Day01 extends Day {
+
+    static {
+        currentDay = new Day01();
+    }
+
+    public static void main(String[] args) {
+        Day.main(args);
+    }
+
+    protected Day01() {
+        super(1);
+    }
+
+    @Override
+    public String part1(List<String> input) {
+        return null;
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        return null;
+    }
+
+}
