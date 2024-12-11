bb7bf7ab0c3bbec0be362649f70c3d8d57aa3f4a
Syed <72160793+ssa17@users.noreply.github.com>
2024-12-11 18:01:26 +0000

    Day 4, initial commit

---
+++ src/main/java/aoc/Day04.java
@@ -0,0 +1,29 @@
+package aoc;
+
+import java.util.List;
+
+public class Day04 extends Day {
+
+    static {
+        currentDay = new Day04();
+    }
+
+    public static void main(String[] args) {
+        Day.main(args);
+    }
+
+    protected Day04() {
+        super(4);
+    }
+
+    @Override
+    public String part1(List<String> input) {
+        return null;
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        return null;
+    }
+
+}
---
+++ src/test/java/aoc/Day04Test.java
@@ -0,0 +1,5 @@
+package aoc;
+
+class Day04Test {
+
+}
